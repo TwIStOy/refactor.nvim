818d170e83363247781af439af29c9ff7541c051
Hawtian Wang <twistoy.wang@gmail.com>
2024-04-19 16:23:03 +0800

    feat(utils): export submodule

--- lua/refactor/utils/init.lua
+++ lua/refactor/utils/init.lua
@@ -7,4 +7,7 @@ M.treesitter = require("refactor.utils.treesitter")
 ---@type refactor.utils.typed
 M.typed = require("refactor.utils.typed")
 
+---@type refactor.utils.vim
+M.vim = require("refactor.utils.vim")
+
 return M
