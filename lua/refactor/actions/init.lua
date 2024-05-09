b2d852917859f34fab09d75adea7be5cab5702c6
Hawtian Wang <twistoy.wang@gmail.com>
2024-05-09 16:26:30 +0800

    feat(actions): create contexts for different filetypes

--- lua/refactor/actions/init.lua
+++ lua/refactor/actions/init.lua
@@ -12,4 +12,17 @@ M.Nix = require("refactor.actions.nix")
 ---@field do_refactor fun(): nil
 ---@field available fun(): boolean
 
+local ft_to_actions = {
+  nix = M.Nix,
+  cpp = M.Cpp,
+}
+
+function M.create_context(ft)
+  local creater = ft_to_actions[ft]
+  if creater == nil then
+    return {}
+  end
+  return creater.create_context()
+end
+
 return M
